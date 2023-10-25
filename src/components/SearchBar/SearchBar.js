fa2c6b13ca75234c2647537e4a2121293e05b081
cyouh95 <cyouh95@g.ucla.edu>
2023-10-25 15:02:53 -0700

    add states to SearchBar

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,25 +1,51 @@
+import { useState } from 'react';
 import './SearchBar.css';
 
 function SearchBar() {
-    const handleClick = (e) => {
+    // Sort by
+    const [sortBy, setSortBy] = useState('best_match');
+
+    function handleSortBy(e) {
         for (const child of e.target.parentElement.children) {
             child.classList.remove('active');
         }
         e.target.classList.add('active');
-    };
+
+        setSortBy(e.target.getAttribute('data-sort'));
+    }
+
+    // Search term
+    const [searchTerm, setSearchTerm] = useState('');
+
+    function handleSearchTerm(e) {
+        setSearchTerm(e.target.value);
+    }
+
+    // Search location
+    const [searchLocation, setSearchLocation] = useState('');
+
+    function handleSearchLocation(e) {
+        setSearchLocation(e.target.value);
+    }
+
+    // Submit
+    function handleSubmit(e) {
+        e.preventDefault();
+        console.log(`Searching Yelp with ${searchTerm}, ${searchLocation}, ${sortBy}...`);
+    }
 
     return (
         <div className="hero">
             <div className="sort">
-                <div className="active" onClick={handleClick}>Best Match</div>
-                <div onClick={handleClick}>Highest Rated</div>
-                <div onClick={handleClick}>Most Reviewed</div>
+                <div className="active" data-sort="best_match" onClick={handleSortBy}>Best Match</div>
+                <div data-sort="rating" onClick={handleSortBy}>Highest Rated</div>
+                <div data-sort="review_count" onClick={handleSortBy}>Most Reviewed</div>
             </div>
             <div className="search">
-                <input type="text" placeholder="Search Businesses" />
-                <input type="text" placeholder="Where?" />
+                <input type="text" onChange={handleSearchTerm} placeholder="Search Businesses" />
+                <input type="text" onChange={handleSearchLocation} placeholder="Where?" />
             </div>
-            <button type="submit">Let's Go</button>
+            <button type="submit" onClick={handleSubmit}>Let's Go</button>
         </div>
     );
 }
